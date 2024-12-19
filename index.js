7cdd8ae8674dcec00270c9b245fe47e8a203a5d6
unknown <nagilapereira8896@digitalcollege.internal>
2024-12-19 12:10:53 -0300

    condicionais correção

---
+++ index.js
@@ -0,0 +1,45 @@
+// if(2 % 3 == 0 ){
+//     console.log( 'é par');   
+// } else {
+//     console.log('é impar');  
+// } 
+
+// function verificarsemaforo(){
+//     if (semaforo.value == 'verde'){
+//       console.log("Siga em frente");
+//     } else if (semaforo.value == 'amarelo') {
+//         console.log("Reduza a velocidade");
+//     } else {
+//         console.log("Pare");
+//     }   
+// }
+
+// function mostrarValorDoImput(){
+//     console.log(TextDecoder.value);
+    
+// }
+
+function gerarNome(){
+    if (dispatchEvent.value == ""){
+        alert("Informe o dia");
+        return;
+    }
+}
+let nome = "";
+
+if (dispatchEvent.value >=1 && dia.value <=8){
+    nome = "Desenvolvedor (a)"
+    console.log ("Nome: ,nome");
+}
+else if (dispatchEvent.value >=9 && dia.value <=18){
+    nome = "Programador (a)"
+    console.log ("Nome: ,nome");
+}
+else if (dispatchEvent.value >=9 && dia.value <=18){
+    nome = "Programador (a)"
+    console.log ("Nome: ,nome");
+}
+else if (dispatchEvent.value >=9 && dia.value <=18){
+    nome = "Programador (a)"
+    console.log ("Nome: ,nome");
+}
